6d09b5df90864b0857d0b133cc1c80e8d37f652e
James Swash <james.swash@hotmail.co.uk>
2022-09-27 22:20:17 +0100

    formatting fixes

--- main.c
+++ main.c
@@ -3,58 +3,53 @@
 
 #define MAX_WORDS 25
 
+int main(void) {
 
-int main(void){
+  printf("\t\tPredictive Text Application\n");
 
-printf("\t\tPredictive Text Application\n");
+  struct Node * trie = NodeConstructor();
 
-struct Node* trie = NodeConstructor();
+  FILE * ptr_file;
+  char buf[25];
 
-FILE *ptr_file;
-    		char buf[25];
+  ptr_file = fopen("words.txt", "r");
+  if (!ptr_file)
+    return 1;
 
-    		ptr_file =fopen("words.txt","r");
-    		if (!ptr_file)
-        		return 1;
-
-    		while (fgets(buf,25, ptr_file)!=NULL){
-                int i;
-                for(i = 0; buf[i]; i++){
-                    buf[i] = tolower(buf[i]);
-                }
-        //       	printf("%s %d",buf, i);
-                insert(trie,buf);
-        		}
-		fclose(ptr_file);
-		printf("file loaded");
-
+  while (fgets(buf, 25, ptr_file) != NULL) {
+    int i;
+    for (i = 0; buf[i]; i++) {
+      buf[i] = tolower(buf[i]);
+    }
+    //       	printf("%s %d",buf, i);
+    insert(trie, buf);
+  }
+  fclose(ptr_file);
+  printf("file loaded");
 
   int letter;
 
-  puts ("Press any key, ':' to show suggestions and '@' quit");
+  puts("Press any key, ':' to show suggestions and '@' quit");
 
   char searchWord[30] = "";
   int i = 0;
 
-  while ((letter = getch()) != EOF && letter!= '@' && i < 29){
-    if(letter != ':'){
-    searchWord[i] = (char)letter;
-    searchWord[i] = tolower(searchWord[i]);
-    printf ("%c", letter);
-    i++;
-    }
-    else {
-        searchWord[i] = '\0';
-        Nodesuggest(trie,searchWord,0);
+  while ((letter = getch()) != EOF && letter != '@' && i < 29) {
+    if (letter != ':') {
+      searchWord[i] = (char) letter;
+      searchWord[i] = tolower(searchWord[i]);
+      printf("%c", letter);
+      i++;
+    } else {
+      searchWord[i] = '\0';
+      Nodesuggest(trie, searchWord, 0);
 
-        }
+    }
 
   }
 
+  nodeFree(trie);
 
-nodeFree(trie);
-
-return 0;
+  return 0;
 
 }
-
